9e7fff2f09957ea5f9b77e85642e82d12ff9c487
phennapa-saeliw <35793853+phennapa-saeliw@users.noreply.github.com>
2023-02-09 01:31:02 +0700

    fix: change constants

--- app/client/src/widgets/NotificationButtonWidget/constants.ts
+++ app/client/src/widgets/NotificationButtonWidget/constants.ts
@@ -1,2 +1,5 @@
-// This file contains common constants which can be used across the widget configuration file (index.ts), widget and component folders.
-export const NOTIFICATIONBUTTON_WIDGET_CONSTANT = "";
+export type ButtonStyle =
+  | "PRIMARY_BUTTON"
+  | "SECONDARY_BUTTON"
+  | "SUCCESS_BUTTON"
+  | "DANGER_BUTTON";
